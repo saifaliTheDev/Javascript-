e29bb7aaa14c0a697e642ad7b6bd00a5bdc69128
SAIF Ali <143219742+saifaliTheDev@users.noreply.github.com>
2024-05-17 12:31:03 +0500

    Add files via upload

---
+++ Class 03/js/script.js
@@ -0,0 +1,110 @@
+// Data types = 8
+// Strings = '' OR " "
+// Numbers = 0123456789
+// Boolean = true or false
+// Function = function sum2Numbers(){}
+// sum2Numbers();
+// undefined
+// Object
+// Array
+// null
+
+
+var firstName = "Saif"
+var lastName = "Ali"
+var fullName = firstName + " " + lastName
+console.log(fullName);
+
+var num1 = "10"
+var num2 = "20"
+
+var sum = num1 + num2;
+console.log(sum);
+
+// Concatenation +
+
+//-----------------
+
+var num1 = "10"
+var num2 = "20"
+
+var sum = num1 - num2;
+console.log(sum);
+
+var num1 = 10
+var num2 = "20"
+
+var sum = num1 + num2;
+console.log(sum);
+
+var isJavascriptEasy = true
+console.log(isJavascriptEasy);
+
+var isJavascriptEasy = "true"
+console.log(isJavascriptEasy);
+
+
+// var abc = "abc"
+// var abc = 1
+// var abc = true
+// var abc = function(){}
+
+var abc;
+console.log(abc);
+
+//  Undefined : variable ko declare krdia, lekin koi value innitialize nhi ki 
+
+var abc = "Saif"
+console.log(typeof abc);
+
+console.log(typeof 5);
+console.log(typeof true);
+
+var userName = "";
+var userAge = 20;
+var userDOB = "13-March-2004";
+var userCountry = "";
+
+// agar aik he variable ka bare me multiple data chahye ho to hum phir object DATA Type ka use krte hain.
+
+// For example:
+
+
+
+// jab aik he specific item ke related data chahte ho to hm phir object k use krte hain.
+// var user = {
+//     name: "Saif",
+//     age:20,
+//     dob:"",
+//     address: "",
+//     country:""
+// }
+
+// console.log(user.name, user.age);
+
+// Arrays: jb same data chahye aik field ka related
+var country0 = 'Pakistan';
+var country1 = 'India';
+var country2 = 'Afghanistan';
+
+// 
+
+var country = ["Pakistan", "India", "Afghanistan"]
+
+
+// we can use 8 of 8 data types in an array.
+
+var users = ["", 0,1, true, false, undefined, function(){}, [[]]]
+
+
+var abc = null;
+
+var user;
+
+// console.log(user.name);
+
+console.log(abc);
+
+
+
+
