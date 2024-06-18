4aac110ba5b368d6d8c803ec095b984280744c51
SAIF Ali <143219742+saifaliTheDev@users.noreply.github.com>
2024-06-18 09:09:47 +0500

    Add files via upload

--- Class 07/JS/script.js
+++ Class 07/JS/script.js
@@ -29,3 +29,112 @@ function capitalize() {
   document.getElementById("output").style.textTransform = "capitalize"; // styling apply on element
   document.getElementById("output").innerHTML = originalText;
 }
+
+//better Formatting
+
+function betterFormatting() {
+  let text = document.getElementById("inputText").value;
+  if (!text) {
+    alert("Please type your text...");
+    return;
+  }
+  // text = text.toLowerCase();
+  document.getElementById("output").style.textTransform = "capitalize";
+  document.getElementById("output").innerHTML = text;
+}
+
+// print all cities
+
+var cities = [
+  "Faisalabad",
+  "Lahore",
+  "Isalamabad",
+  "Karachi",
+  "Multan",
+  "Sheikupura",
+  "Jhang",
+  "Sargodha",
+];
+
+function printCities() {
+  document.getElementById("output").innerHTML = " ";
+  for (i = 0; i < cities.length; i++) {
+    let num = i + 1;
+    document.getElementById("output").innerHTML +=
+      num + ") " + cities[i] + "<br/>";
+  }
+}
+
+// Add your City
+
+// function addCity() {
+//   let city = document.getElementById("inputText").value;
+
+//   if (city.length < 3) {
+//     alert("Please type your name correctly.");
+//     return;
+//   }
+
+//   let cityFirstLetter = city.charAt(0).toUpperCase();
+//   let cityAllLetters = city.slice(1).toLowerCase();
+//   let cityWordInCapitalize = cityFirstLetter + cityAllLetters;
+//   // console.log(cityWordInCapitalize);
+//   // cities.push(cityWordInCapitalize);
+
+//   let cityFound = false;
+
+//   for (i = 0; i < cities.length; i++) {
+
+//     if (cities[i] === cityWordInCapitalize) {
+//       cityFound = true;
+
+//       let html =
+//         '<span style="color: red; font-size: 20px;"> "' +
+//         cityWordInCapitalize +
+//         '" </span> is already in list.';
+
+//       document.getElementById("output").innerHTML = html;
+//     }
+//   }
+
+//   if (cityFound = false) {
+//     cities.push(cityWordInCapitalize);
+//     let html =
+//       '<span style="color: green; font-size: 20px;"> "' +
+//       cityWordInCapitalize +
+//       '" </span> has been successfully added in list.';
+//     document.getElementById("output").innerHTML = html;
+//   }
+// }
+
+function addCity() {
+  let city = document.getElementById("inputText").value;
+
+  if (city.length < 3) {
+    alert("Please type your name correctly.");
+    return;
+  }
+
+  let cityFirstLetter = city.charAt(0).toUpperCase();
+  let cityAllLetters = city.slice(1).toLowerCase();
+  let cityWordInCapitalize = cityFirstLetter + cityAllLetters;
+  console.log(cityWordInCapitalize);
+  // cities.push(cityWordInCapitalize);
+
+  let cityFound = false;
+
+  for (let i = 0; i < cities.length; i++) {
+    if (cities[i] === cityWordInCapitalize) {
+      cityFound = true;
+      let html = '<span style="color: red; font-size: 20px;"> "'+ cityWordInCapitalize +'" </span> is already in list.';
+      document.getElementById("output").innerHTML = html;
+    }
+  }
+
+  if (cityFound === false){
+    cities.push(cityWordInCapitalize);
+    let html = '<span style="color: dark green; font-size: 20px;"> "'+ cityWordInCapitalize +'" </span> has been successfully added in list.';
+    document.getElementById("output").innerHTML = html;
+  }
+}
+
